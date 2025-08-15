eefbfbad75979540761fd79d686c3bfaba04628e
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-08-14 20:17:10 -0700

    add: typescript ts-node and @types/node library

--- test/src/index.ts
+++ test/src/index.ts
@@ -1,2 +1,14 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
